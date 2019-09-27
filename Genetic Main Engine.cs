bd8b677cb713925d6c2b54664002f0f9e8e2622f
musicandelectronics <41381526+musicandelectronics@users.noreply.github.com>
2019-09-27 14:50:04 +0900

    first commit
    first commit for boot 4dim Konkuk University Graduation Exhibition

---
+++ Genetic Main Engine.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Microsoft.Win32;
+
+namespace Genetic_AI
+{
+    class Genetic_Main_Engine
+    {
+        public string raw_json_data { get; set; }
+        public JObject genetic_raw_object { get; set; }
+
+        public List<int[]> gene_set;
+        public List<int[]> survived_genes;
+
+        Random random_generator = new Random();
+
+        public int[] genetic_target_data { get; set; }
+        public int generation;
+
+        public void Json_parser()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            Nullable<bool> result = openFileDialog.ShowDialog();
+            raw_json_data = File.ReadAllText(openFileDialog.FileName);
+            genetic_raw_object = JObject.Parse(raw_json_data);
+
+        }
+
+        public void Gene_Initialize()
+        {
+            generation = 0;
+            gene_set = new List<int[]>();
+
+            genetic_target_data = new int[(int)genetic_raw_object["Genetic Length"]];
+
+            Gene_generator();
+
+            for (int i = 0; i < (int)genetic_raw_object["Genetic Length"]; i++)
+            {
+                genetic_target_data[i] = random_generator.Next((int)genetic_raw_object["Genetic Range"][0], (int)genetic_raw_object["Genetic Range"][1]);
+            }
+
+        }
+
+       public  void Gene_generator()
+        {
+            for (int i = 0; i < (int)genetic_raw_object["Entity"]; i++)
+            {
+                int[] genetic_member = new int[(int)genetic_raw_object["Genetic Length"]];
+
+                for (int k = 0; k < genetic_member.Length; k++)
+                {
+                    genetic_member[k] = random_generator.Next((int)genetic_raw_object["Genetic Range"][0], (int)genetic_raw_object["Genetic Range"][1]);
+                }
+                gene_set.Add(genetic_member);
+            }
+        }
+
+       public void Gene_generator(List<int[]> survived_genes)
+        {
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int k = 0; k < survived_genes[0].Length; k++)
+                {
+                    gene_set[i][k] = survived_genes[i][k];
+                }
+            }
+
+            for (int i = 2; i < gene_set.Count - 1; i++)
+            {
+                int genetic_mix_rand_num = random_generator.Next(survived_genes[0].Length);
+                double mutant_probabilty = (double)genetic_raw_object["Mutant Probability"] * 100;
+
+                if (random_generator.Next(1,100)<=mutant_probabilty)
+                {
+                    for (int k = 0; k < genetic_mix_rand_num; k++)
+                    {
+                        gene_set[i][k] = survived_genes[0][k];
+                    }
+
+                    for (int k = genetic_mix_rand_num; k < survived_genes[0].Length; k++)
+                    {
+                        gene_set[i][k] = survived_genes[1][k];
+                    }
+
+                    gene_set[i][genetic_mix_rand_num] = random_generator.Next((int)genetic_raw_object["Genetic Range"][0], (int)genetic_raw_object["Genetic Range"][1]);
+                }
+                else if(random_generator.Next(1,100) <100 && random_generator.Next(1,100) >=50)
+                {
+                    for (int k = 0; k < genetic_mix_rand_num / 2; k++)
+                    {
+                        gene_set[i][genetic_mix_rand_num / 2+ k] = survived_genes[0][k];
+                    }
+
+                    for (int k = genetic_mix_rand_num / 2; k < genetic_mix_rand_num; k++)
+                    {
+                        gene_set[i][k- genetic_mix_rand_num / 2] = survived_genes[1][k];
+                    }
+                }
+                else
+                {
+                    for (int k = 0; k < genetic_mix_rand_num / 2; k++)
+                    {
+                        int odd = 2 * k + 1;
+                        gene_set[i][odd] = survived_genes[0][k];
+                    }
+
+                    for (int k = 1; k < genetic_mix_rand_num / 2; k++)
+                    {
+                        int even = 2 * k;
+                        gene_set[i][even] = survived_genes[1][k];
+                    }
+                }
+            }
+
+            survived_genes.RemoveAt(0);
+            survived_genes.RemoveAt(0);
+        }
+
+        public void Survive(List<int[]> gene_set, int[] genetic_target_data)
+        {
+
+            survived_genes = new List<int[]>();
+
+            List<Score_Data> Score = new List<Score_Data>();
+
+            foreach (int[] gene in gene_set)
+            {
+                Score.Add(new Score_Data(Score_Calculation(genetic_target_data, gene), gene));
+            }
+
+            Score.Sort(
+                delegate (Score_Data A, Score_Data B)
+                {
+                    if (A.score > B.score) return 1;
+                    else if (A.score == B.score) return 0;
+                    else return -1;
+                }
+            );
+
+            survived_genes.Add((Score[0].gene_data));
+            survived_genes.Add((Score[1].gene_data));
+            generation++;
+
+        }
+
+        public int Score_Calculation(int[] goal_gene, int[] operand_gene)
+        {
+            int temp_sum = 0;
+            
+            for (int i = 0; i < goal_gene.Length; i++)
+            {
+                temp_sum += (goal_gene[i] - operand_gene[i]) * (goal_gene[i] - operand_gene[i]);
+            }
+            return temp_sum;
+        }
+
+        public bool total_Score_Caculation(int[] goal_gene,List<int[]>operand_gene)
+        {
+            int temp_sum = 0;
+            foreach(int [] gene in operand_gene)
+            {
+                temp_sum+=Score_Calculation(goal_gene, gene);
+            }
+            if (temp_sum == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public class Score_Data
+    {
+        public int score { get; set; }
+        public int[] gene_data { get; set; }
+
+        public Score_Data(int tmp_score, int[] tmp_gene_data)
+        {
+            gene_data = tmp_gene_data;
+            score = tmp_score;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,186 @@
+﻿using System.Windows;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Threading.Tasks;
+namespace Genetic_AI
+{
+    public partial class MainWindow : Window
+    {
+        Genetic_Main_Engine genetic_Main_Engine = new Genetic_Main_Engine();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Json_Button_Click(object sender, RoutedEventArgs e)
+        {
+            textbox_Json_Entity.Clear();
+            textbox_Json_Genetic_Range.Clear();
+            textbox_Json_Gene_Length.Clear();
+            textbox_Json_Mutant_Prob.Clear();
+            genetic_Main_Engine.Json_parser();
+            textbox_Json_Entity.AppendText((string)genetic_Main_Engine.genetic_raw_object["Entity"]);
+            textbox_Json_Genetic_Range.AppendText
+                (
+                (string)genetic_Main_Engine.genetic_raw_object["Genetic Range"][0]
+                + "~" +
+                (string)genetic_Main_Engine.genetic_raw_object["Genetic Range"][1]
+                );
+            textbox_Json_Gene_Length.AppendText((string)genetic_Main_Engine.genetic_raw_object["Genetic Length"]);
+            textbox_Json_Mutant_Prob.AppendText(((double)genetic_Main_Engine.genetic_raw_object["Mutant Probability"] * 100).ToString() + "%");
+        }
+
+        private void Initialize_Gene_Click(object sender, RoutedEventArgs e)
+        {
+            Canvas1.Children.Clear();
+            genetic_Main_Engine.Gene_Initialize();
+            int index = 0;
+
+            textblock_TargetData.Text = $"Target Data\nGeneration : {genetic_Main_Engine.generation.ToString()}";
+            textblock_Data.Text = "Data";
+            for (int i = 0; i < genetic_Main_Engine.genetic_target_data.Length; i++)
+            {
+
+                Canvas1.Children.Add(Create_Rectangle(i, Canvas1.Margin.Bottom - 140, genetic_Main_Engine.genetic_target_data[i], 1));
+
+            }
+
+            foreach (int[] gene in genetic_Main_Engine.gene_set)
+            {
+                for (int i = 0; i < gene.Length; i++)
+                {
+                    Canvas1.Children.Add(Create_Rectangle(i, index, gene[i], 0));
+                }
+                index++;
+            }
+
+        }
+
+        Rectangle Create_Rectangle(double desiredCenterX, double desiredCenterY, int color, int target)
+        {
+            int bytecolor ;
+            double length_ratio =  1 /  (double)genetic_Main_Engine.genetic_raw_object["Genetic Length"] ;
+            double depth_ratio= 1/  ((double)genetic_Main_Engine.genetic_raw_object["Entity"]*1.2);
+            double betweensize;
+            double vertical_gap;
+        
+            if (target == 0)
+            {
+                bytecolor = (color*1515) % 255;
+                betweensize = desiredCenterX * 1000 * length_ratio;
+                vertical_gap = desiredCenterY * 1500 * depth_ratio + Canvas1.Margin.Bottom;
+            }
+
+            else
+            {
+                bytecolor = (color*1515) % 255;
+                betweensize = desiredCenterX * 1000 * length_ratio;
+                vertical_gap = desiredCenterY-150;
+            }
+
+            Rectangle rectangle = new Rectangle { Width = 600 * length_ratio, Height = 600 * depth_ratio, Fill = new SolidColorBrush(Color.FromRgb((byte)bytecolor, 100 ,100)) };
+            double left = betweensize - (5 / 2);
+            double bottom = -vertical_gap - 500;
+            rectangle.Margin = new Thickness(10+ left, 0, 0, bottom);
+            return rectangle;
+        }
+
+        async private void Goto_Next_Generation_Click(object sender, RoutedEventArgs e)
+        {
+
+            while (genetic_Main_Engine.Score_Calculation(genetic_Main_Engine.genetic_target_data, genetic_Main_Engine.gene_set[0]) >0)
+            {
+                genetic_Main_Engine.Survive(genetic_Main_Engine.gene_set, genetic_Main_Engine.genetic_target_data);
+                genetic_Main_Engine.Gene_generator(genetic_Main_Engine.survived_genes);
+                int entity = (int)genetic_Main_Engine.genetic_raw_object["Entity"];
+                int index = 0;
+                textbox_GeneList.Clear();
+                await Task.Run(() =>
+                {
+                    Canvas1.Dispatcher.Invoke(()
+                         => Canvas1.Children.Clear(), System.Windows.Threading.DispatcherPriority.Background);
+                    
+                    Canvas1.Dispatcher.Invoke(()
+                      => textblock_TargetData.Text= $"Target Data\nGeneration : {genetic_Main_Engine.generation.ToString()}", System.Windows.Threading.DispatcherPriority.Background);
+
+                    Canvas1.Dispatcher.Invoke(()
+                          => textblock_Data.Text= "Data", System.Windows.Threading.DispatcherPriority.Background);
+                    Canvas1.Dispatcher.Invoke(()
+                    =>
+                    {
+                        for (int i = 0; i < genetic_Main_Engine.genetic_target_data.Length; i++)
+                        {
+                            Canvas1.Children.Add(Create_Rectangle(i, Canvas1.Margin.Bottom - 140, genetic_Main_Engine.genetic_target_data[i], 1));
+                            
+                        }
+                    }
+                    , System.Windows.Threading.DispatcherPriority.Background);
+                    Canvas1.Dispatcher.Invoke(()
+                     =>
+                    {
+                        foreach (int[] gene in genetic_Main_Engine.gene_set)
+                        {
+                            int score = genetic_Main_Engine.Score_Calculation(genetic_Main_Engine.genetic_target_data, gene);
+                            Canvas1.Dispatcher.Invoke(()
+                     =>
+                            {
+                                for (int i = 0; i < gene.Length; i++)
+                                {
+                                    Rectangle rec = Create_Rectangle(i, index, gene[i], 0);
+                                    Canvas1.Children.Add(rec);
+                                }
+                                textbox_GeneList.AppendText($"index{index} Score: " + genetic_Main_Engine.Score_Calculation(gene, genetic_Main_Engine.genetic_target_data).ToString() + "\n");
+
+                            }, System.Windows.Threading.DispatcherPriority.Background
+                        );
+                            index++;
+                        }
+                    }, System.Windows.Threading.DispatcherPriority.Background
+                        );
+                }
+              );
+            }
+            MessageBox.Show("Calculation Completed!");
+        }
+
+        private void Goto_Manual_Next_Generation_Click(object sender, RoutedEventArgs e)
+        {
+            genetic_Main_Engine.Survive(genetic_Main_Engine.gene_set, genetic_Main_Engine.genetic_target_data);
+            genetic_Main_Engine.Gene_generator(genetic_Main_Engine.survived_genes);
+            int entity = (int)genetic_Main_Engine.genetic_raw_object["Entity"];
+            int index = 0;
+
+            Canvas1.Children.Clear();
+            textbox_GeneList.Clear();
+            textblock_TargetData.Text = $"Target Data\nGeneration : {genetic_Main_Engine.generation.ToString()}";
+            textblock_Data.Text = "Data";
+
+            for (int i = 0; i < genetic_Main_Engine.genetic_target_data.Length; i++)
+            {
+                Canvas1.Children.Add(Create_Rectangle(i, Canvas1.Margin.Bottom - 140, genetic_Main_Engine.genetic_target_data[i], 1));
+            }
+
+            foreach (int[] gene in genetic_Main_Engine.gene_set)
+            {
+                int score = genetic_Main_Engine.Score_Calculation(genetic_Main_Engine.genetic_target_data, gene);
+
+                for (int i = 0; i < gene.Length; i++)
+                {
+                    Rectangle rec = Create_Rectangle(i, index, gene[i], 0);
+                    Canvas1.Children.Add(rec);
+
+                }
+                textbox_GeneList.AppendText($"index{index} Score: " + genetic_Main_Engine.Score_Calculation(gene, genetic_Main_Engine.genetic_target_data).ToString() + "\n");
+
+                index++;
+            }
+            if (genetic_Main_Engine.Score_Calculation(genetic_Main_Engine.genetic_target_data, genetic_Main_Engine.gene_set[0]) == 0)
+                MessageBox.Show("Calculation Completed!");
+        }
+    }
+}
+
+
+
+
